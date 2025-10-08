b45b2e62588e6e35859fb2e1d2a65d945d8497fc
Ryan <ryan@3rdshiftstudios.com>
2025-10-07 17:00:22 -0700

    fudge

--- Assets/TPSBR/Scripts/Gameplay/Agent/Character/States/JumpState.cs
+++ Assets/TPSBR/Scripts/Gameplay/Agent/Character/States/JumpState.cs
@@ -1,13 +1,18 @@
+using Fusion.Addons.KCC;
+using UnityEngine;
+
 namespace TPSBR
 {
 	using Fusion.Addons.AnimationController;
 
-	public class JumpState : MirrorBlendTreeState
+	public class JumpState : ClipState
 	{
 		// PRIVATE MEMBERS
 
 		private Inventory _inventory;
-
+		private KCC     _kcc;
+		private Agent   _agent;
+		
 		// AnimationState INTERFACE
 
 		protected override void OnInitialize()
