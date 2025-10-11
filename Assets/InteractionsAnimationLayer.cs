21f7bc7e1e353c429054369cf3cc16bd73313132
Ryan Barton <133931120+ThirdShiftStudios@users.noreply.github.com>
2025-10-10 22:19:17 -0700

    Fix lazy lookup for chest animation state

--- Assets/InteractionsAnimationLayer.cs
+++ Assets/InteractionsAnimationLayer.cs
@@ -6,8 +6,29 @@ namespace TPSBR
     public class InteractionsAnimationLayer : AnimationLayer
     {
         [SerializeField]
-        private OpenChestState    _openChest;
+        private OpenChestState _openChest;
 
-        public OpenChestState OpenChest => _openChest;
+        public OpenChestState OpenChest
+        {
+            get
+            {
+                if (_openChest == null)
+                {
+                    FindState(out _openChest, true);
+                }
+
+                return _openChest;
+            }
+        }
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            if (_openChest == null)
+            {
+                FindState(out _openChest, true);
+            }
+        }
+#endif
     }
 }
