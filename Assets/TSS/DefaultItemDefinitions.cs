0a403468899e26e733f494bbce778f6d1b7287bf
Ryan <ryan@3rdshiftstudios.com>
2025-10-12 12:46:19 -0700

    added default items and serialized default pickaxe

---
+++ Assets/TSS/DefaultItemDefinitions.cs
@@ -0,0 +1,14 @@
+﻿using Unity.Template.CompetitiveActionMultiplayer;
+using UnityEngine;
+
+namespace TPSBR
+{
+    public class DefaultItemDefinitions : GlobalDataDefinition
+    {
+        public override string Name => "Default Items";
+        public override Texture2D Icon { get; }
+
+        [SerializeField] 
+        private PickaxeDefinition _defaultPickaxe;
+    }
+}
---
+++ Assets/TSS/GlobalDataDefinition.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using TSS.Data;
+
+namespace TPSBR
+{
+    public abstract class GlobalDataDefinition : DataDefinition
+    {
+        
+    }
+}
