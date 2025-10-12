c6a2c5b74cf062cf5b19c57fe6449f9d6d2ceedc
Ryan <ryan@3rdshiftstudios.com>
2025-10-12 14:46:08 -0700

    added wood axe

---
+++ Assets/TPSBR/Scripts/Gameplay/Weapons/WoodAxe.cs
@@ -0,0 +1,13 @@
+﻿using Unity.Template.CompetitiveActionMultiplayer;
+using UnityEngine;
+
+namespace TPSBR
+{
+    public class WoodAxe : Tool
+    {
+        [SerializeField]
+        private WoodAxeDefinition _woodAxeDefinition;
+
+        public WoodAxeDefinition Definition => _woodAxeDefinition;
+    }
+}
---
+++ Assets/TSS/WoodAxeDefinition.cs
@@ -0,0 +1,14 @@
+using TPSBR;
+using TSS.Data;
+using UnityEngine;
+
+namespace Unity.Template.CompetitiveActionMultiplayer
+{
+    public class WoodAxeDefinition : ItemDefinition
+    {
+        [SerializeField]
+        private WoodAxe _woodAxePrefab;
+        public WoodAxe WoodAxePrefab => _woodAxePrefab;
+        public override ushort MaxStack => 1;
+    }
+}
