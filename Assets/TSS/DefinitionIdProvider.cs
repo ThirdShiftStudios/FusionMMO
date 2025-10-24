4e4b6413f90b1bf3d9da9c19742b731c07736739
Ryan Barton <133931120+ThirdShiftStudios@users.noreply.github.com>
2025-10-24 16:47:29 -0700

    Prevent DefinitionIdProvider from rewriting without new IDs

--- Assets/TSS/DefinitionIdProvider.cs
+++ Assets/TSS/DefinitionIdProvider.cs
@@ -1,5 +1,6 @@
 // Assets/TSS/Editor/DefinitionIdProvider.cs
 #if UNITY_EDITOR
+using System;
 using UnityEditor;
 using UnityEngine;
 
@@ -8,19 +9,50 @@ namespace TSS.Data
     [FilePath("ProjectSettings/TSS_DefinitionIdProvider.asset", FilePathAttribute.Location.ProjectFolder)]
     public sealed class DefinitionIdProvider : ScriptableSingleton<DefinitionIdProvider>
     {
-        public int LastAssignedId = -1;
+        [SerializeField]
+        private int lastAssignedId = -1;
+
+        public int LastAssignedId => lastAssignedId;
 
         public int GetNextIdAndPersist()
         {
-            unchecked
+            var maxExistingId = Math.Max(lastAssignedId, GetCurrentMaxDefinitionId());
+            var nextId = maxExistingId + 1;
+
+            if (nextId != lastAssignedId)
             {
-                LastAssignedId = LastAssignedId < -1 ? -1 : LastAssignedId;
-                LastAssignedId++;
+                lastAssignedId = nextId;
+                Persist();
             }
 
-            // Save() is protected; OK to call here.
-            Save(true);
-            return LastAssignedId;
+            return lastAssignedId;
+        }
+
+        private int GetCurrentMaxDefinitionId()
+        {
+            var guids = AssetDatabase.FindAssets($"t:{nameof(DataDefinition)}");
+            var maxId = -1;
+
+            foreach (var guid in guids)
+            {
+                var assetPath = AssetDatabase.GUIDToAssetPath(guid);
+                var definition = AssetDatabase.LoadAssetAtPath<DataDefinition>(assetPath);
+
+                if (definition == null)
+                {
+                    continue;
+                }
+
+                maxId = Mathf.Max(maxId, definition.ID);
+            }
+
+            return maxId;
+        }
+
+        private void Persist()
+        {
+            EditorUtility.SetDirty(this);
+            AssetDatabase.SaveAssetIfDirty(this);
         }
     }
 }
