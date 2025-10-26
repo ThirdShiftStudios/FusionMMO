690ae3b61268c802ccbad96b4f8e0173603e83c8
Ryan <ryan@3rdshiftstudios.com>
2025-10-26 16:37:52 -0700

    added fish setup

--- Assets/TSS/FishDefinition.cs
+++ Assets/TSS/FishDefinition.cs
@@ -5,18 +5,11 @@ using UnityEngine;
 namespace TPSBR
 {
     [CreateAssetMenu(fileName = "FishDefinition", menuName = "TSS/Data Definitions/Fish")]
-    public sealed class FishDefinition : DataDefinition
+    public sealed class FishDefinition : ItemDefinition
     {
-        [SerializeField]
-        private string _displayName;
-        [SerializeField]
-        private Texture2D _icon;
         [SerializeField]
         private FishItem _fishPrefab;
 
         public FishItem FishPrefab => _fishPrefab;
-
-        public override string Name => string.IsNullOrWhiteSpace(_displayName) ? name : _displayName;
-        public override Texture2D Icon => _icon;
     }
 }
