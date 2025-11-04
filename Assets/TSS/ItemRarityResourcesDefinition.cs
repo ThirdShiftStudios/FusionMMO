e051c6e1ffaec7b1fb439b514f7dd961e86658b4
Ryan Barton <133931120+ThirdShiftStudios@users.noreply.github.com>
2025-11-03 16:42:26 -0800

    Add item rarity resources and accessors

---
+++ Assets/TSS/ItemRarityResourcesDefinition.cs
@@ -0,0 +1,80 @@
+using System;
+using UnityEngine;
+
+namespace TSS.Data
+{
+    public enum EItemRarity
+    {
+        None = 0,
+        Common,
+        Uncommon,
+        Rare,
+        Epic,
+        Legendary
+    }
+
+    [Serializable]
+    public struct ItemRarityData
+    {
+        [SerializeField] private EItemRarity _rarity;
+        [SerializeField] private Color _primaryTextColor = Color.white;
+        [SerializeField] private Color _secondaryTextColor = Color.white;
+        [SerializeField] private GameObject _pickupVisuals;
+
+        public EItemRarity Rarity => _rarity;
+        public Color PrimaryTextColor => _primaryTextColor;
+        public Color SecondaryTextColor => _secondaryTextColor;
+        public GameObject PickupVisuals => _pickupVisuals;
+    }
+
+    [CreateAssetMenu(fileName = "ItemRarityResources", menuName = "TSS/Data Definitions/Item Rarity Resources")]
+    public class ItemRarityResourcesDefinition : DataDefinition
+    {
+        private const string ResourcePath = "ItemRarityResources";
+        private static ItemRarityResourcesDefinition _instance;
+
+        [SerializeField] private Texture2D _icon;
+        [SerializeField] private string _displayName = "Item Rarity Resources";
+        [SerializeField] private ItemRarityData[] _rarities = Array.Empty<ItemRarityData>();
+
+        public static ItemRarityResourcesDefinition Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = Resources.Load<ItemRarityResourcesDefinition>(ResourcePath);
+
+#if UNITY_EDITOR || DEVELOPMENT_BUILD
+                    if (_instance == null)
+                    {
+                        Debug.LogWarning($"Unable to locate {nameof(ItemRarityResourcesDefinition)} asset at Resources/{ResourcePath}.");
+                    }
+#endif
+                }
+
+                return _instance;
+            }
+        }
+
+        public override string Name => string.IsNullOrWhiteSpace(_displayName) ? name : _displayName;
+        public override Texture2D Icon => _icon;
+
+        public ItemRarityData[] Rarities => _rarities;
+
+        public bool TryGetData(EItemRarity rarity, out ItemRarityData rarityData)
+        {
+            for (var i = 0; i < _rarities.Length; i++)
+            {
+                if (_rarities[i].Rarity == rarity)
+                {
+                    rarityData = _rarities[i];
+                    return true;
+                }
+            }
+
+            rarityData = default;
+            return false;
+        }
+    }
+}
