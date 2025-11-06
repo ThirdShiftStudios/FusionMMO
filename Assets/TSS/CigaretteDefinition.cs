a9a3eb6e30fbf8446d9de6d755e0bfe337f4a06e
Ryan <ryan@3rdshiftstudios.com>
2025-11-05 19:41:29 -0800

    config

---
+++ Assets/TSS/CigaretteDefinition.cs
@@ -0,0 +1,12 @@
+using TSS.Data;
+using TPSBR.Abilities;
+using Unity.Template.CompetitiveActionMultiplayer;
+using UnityEngine;
+
+namespace TPSBR
+{
+    public class CigaretteDefinition : WeaponDefinition
+    {
+        public override ESlotCategory SlotCategory => ESlotCategory.Consumable;
+    }
+}
