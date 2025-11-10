31c92fcb3cb3c62804affbe2d80d6c346b847b68
Ryan <ryan@3rdshiftstudios.com>
2025-11-09 16:14:11 -0800

    config

--- Assets/TPSBR/Scripts/Core/LoadingScene.cs
+++ Assets/TPSBR/Scripts/Core/LoadingScene.cs
@@ -6,97 +6,101 @@ using TMPro;
 
 namespace TPSBR
 {
-	public class LoadingScene : MonoBehaviour
-	{
-		// PUBLIC MEMBERS
+    public class LoadingScene : MonoBehaviour
+    {
+        // PUBLIC MEMBERS
 
-		public bool IsFading => _activeFader != null && _activeFader.IsFinished == false;
+        public bool IsFading => _activeFader != null && _activeFader.IsFinished == false;
 
-		// PRIVATE MEMBERS
+        // PRIVATE MEMBERS
 
-		[SerializeField]
-		private UIFader _fadeInObject;
-		[SerializeField]
-		private UIFader _fadeOutObject;
-		[SerializeField]
-		private TextMeshProUGUI _status;
-		[SerializeField]
-		private TextMeshProUGUI _statusDescription;
-		[SerializeField]
-                private UIYesNoDialogView _dialog;
+        [SerializeField]
+        private UIFader _fadeInObject;
+        [SerializeField]
+        private UIFader _fadeOutObject;
+        [SerializeField]
+        private TextMeshProUGUI _status;
+        [SerializeField]
+        private TextMeshProUGUI _statusDescription;
+        [SerializeField]
+        private UIYesNoDialogView _dialog;
 
-                [SerializeField]
-                private Image _loadingImage;
+        [SerializeField]
+        private Image[] _loadingImage;
 
-		private UIFader _activeFader;
+        private UIFader _activeFader;
 
-		// PUBLIC METHODS
+        // PUBLIC METHODS
 
-                public void FadeIn()
+        public void FadeIn()
+        {
+            _fadeInObject.SetActive(true);
+            _fadeOutObject.SetActive(false);
+
+            _activeFader = _fadeInObject;
+        }
+
+        public void FadeOut()
+        {
+            _dialog.Close_Internal();
+
+            _fadeInObject.SetActive(false);
+            _fadeOutObject.SetActive(true);
+
+            _activeFader = _fadeOutObject;
+        }
+
+        public void SetLoadingSprite(Sprite sprite)
+        {
+            if (_loadingImage == null)
+                return;
+
+            foreach (var image in _loadingImage)
+            {
+                image.sprite = sprite;
+                image.enabled = sprite != null;
+            }
+
+        }
+
+        // MONOBEHAVIOUR
+
+        protected void Awake()
+        {
+            _fadeInObject.SetActive(false);
+            _fadeOutObject.SetActive(false);
+
+            _dialog.Initialize(null, null);
+        }
+
+        protected void Update()
+        {
+            _status.text = Global.Networking.Status;
+            _statusDescription.text = Global.Networking.StatusDescription;
+
+            if (Keyboard.current.escapeKey.wasPressedThisFrame == true)
+            {
+                _dialog.Open_Internal();
+
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+
+                _dialog.HasClosed += (result) =>
                 {
-                        _fadeInObject.SetActive(true);
-                        _fadeOutObject.SetActive(false);
+                    if (result == true)
+                    {
+                        Global.Networking.StopGame();
+                    }
+                };
+            }
+        }
 
-                        _activeFader = _fadeInObject;
-                }
-
-                public void FadeOut()
-		{
-			_dialog.Close_Internal();
-
-			_fadeInObject.SetActive(false);
-			_fadeOutObject.SetActive(true);
-
-                        _activeFader = _fadeOutObject;
-                }
-
-                public void SetLoadingSprite(Sprite sprite)
-                {
-                        if (_loadingImage == null)
-                                return;
-
-                        _loadingImage.sprite  = sprite;
-                        _loadingImage.enabled = sprite != null;
-                }
-
-		// MONOBEHAVIOUR
-
-		protected void Awake()
-		{
-			_fadeInObject.SetActive(false);
-			_fadeOutObject.SetActive(false);
-
-			_dialog.Initialize(null, null);
-		}
-
-		protected void Update()
-		{
-			_status.text = Global.Networking.Status;
-			_statusDescription.text = Global.Networking.StatusDescription;
-
-			if (Keyboard.current.escapeKey.wasPressedThisFrame == true)
-			{
-				_dialog.Open_Internal();
-
-				Cursor.lockState = CursorLockMode.None;
-				Cursor.visible   = true;
-
-				_dialog.HasClosed += (result) =>
-				{
-					if (result == true)
-					{
-						Global.Networking.StopGame();
-					}
-				};
-			}
-		}
-
-		protected void OnDestroy()
-		{
-			if (_dialog != null)
-			{
-				_dialog.Deinitialize();
-			}
-		}
-	}
+        protected void OnDestroy()
+        {
+            if (_dialog != null)
+            {
+                _dialog.Deinitialize();
+            }
+        }
+    }
 }
